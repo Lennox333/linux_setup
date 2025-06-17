1a2125a5a21e70e9fd2445e5ff0390c671d463a6
Lennox333 <anhnguyen607090@gmail.com>
2025-06-17 16:58:44 +1000

    overhaul

--- .local/bin/icat-float.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-img="$1"
-basename=$(basename "$img")
-name="${basename%.*}-float"
-
-# Open new kitty window with kitten icat
-kitty --title "$name" sh -c "kitten icat '$img'; read -p 'Press enter to close...'"
