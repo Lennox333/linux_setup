5a41995bc5bfceb4169d913880d202f8a60cb03e
Lennox333 <anhnguyen607090@gmail.com>
2025-06-22 19:10:11 +1000

    update

--- .local/setup_scripts/post-install.sh
+++ .local/setup_scripts/post-install.sh
@@ -10,3 +10,6 @@ echo "SDDM enabled and started."
 
 
 echo "env QT_QPA_PLATFORM=xcb pentablet.desktop if you wanna run it"
+
+#SUBSYSTEMS=="usb", ATTRS{idVendor}=="fffe", ATTRS{idProduct}=="0009", TAG+="uaccess"
+#SUBSYSTEM=="hidraw", ATTRS{idVendor}=="fffe", ATTRS{idProduct}=="0009", TAG+="uaccess"
