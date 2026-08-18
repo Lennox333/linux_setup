7bef099067827aa20938a03f49e087aaaf1007bf
Lennox333 <anhnguyen607090@gmail.com>
2026-08-18 20:34:30 +1000

    d

--- .local/bin/fastfetch_ascii.sh
+++ .local/bin/fastfetch_ascii.sh
@@ -12,29 +12,31 @@ ASCII_DIR="$HOME/.config/fastfetch/ascii" # Change as needed
 # Pick a random .txt file
 ASCII_FILE=$(find "$ASCII_DIR" -type f -name "*.txt" | shuf -n 1)
 
-ascii_height=$(wc -l < "$ASCII_FILE")
+#ASCII_FILE="/home/ln607/.config/fastfetch/ascii/waifu.txt"
+#ASCII_FILE="/home/ln607/.config/fastfetch/ascii/nier.txt"
+ascii_height=$(wc -l <"$ASCII_FILE")
 
-# echo $ascii_height
+#echo $ascii_height
 # Terminal dimensions
 # term_width=$(tput cols)
-term_height=$(tput lines)
-# term_height=35
+#term_height=$(tput lines)
+#term_height=35
 # echo $term_height
 # Calculate padding to roughly center
 # padding_left=$(( (term_width - ascii_width) / 2 ))
-padding_top=$(( (term_height - ascii_height ) / 10 ))
+padding_top=$(((ascii_height) % 10))
 
-# echo $padding_top
+#echo $padding_top
 # Ensure non-negative padding
 # padding_left=$((padding_left < 0 ? 0 : padding_left))
-# padding_top=$((padding_top  > 20 ? 2 : padding_top))
-padding_top=$((ascii_height - padding_top  > 12 ? 2 : padding_top))
+padding_top=$((ascii_height > 20 ? 0 : padding_top))
+#padding_top=$((ascii_height - padding_top > 12 ? 2 : padding_top))
 
-# echo $padding_top
+#echo $padding_top
 
 # If a file was found, run fastfetch with it
 if [ -n "$ASCII_FILE" ]; then
-  fastfetch --file "$ASCII_FILE" --logo-padding-left 5 --logo-padding-top "$padding_top"
+  fastfetch --file "$ASCII_FILE" --logo-padding-left 5 --logo-padding-top "$padding_top" --logo-height "$padding_top"
 
 else
   echo "No ASCII .txt files found in $ASCII_DIR"
--- .local/setup_scripts/post-install.sh
+++ .local/setup_scripts/post-install.sh
@@ -19,6 +19,15 @@ echo "env QT_QPA_PLATFORM=xcb pentablet.desktop if you wanna run it"
 #SUBSYSTEM=="hidraw", ATTRS{idVendor}=="fffe", ATTRS{idProduct}=="0009", TAG+="uaccess"
 
 xdg-mime default thunar.desktop inode/directory
+xdg-mime default firefox.desktop x-scheme-handler/https
+xdg-mime default firefox.desktop x-scheme-handler/http
+xdg-mime default org.kde.gwenview.desktop image/png
+xdg-mime default org.kde.gwenview.desktop image/jpeg
+xdg-mime default org.kde.gwenview.desktop image/webp
+xdg-mime default org.kde.gwenview.desktop image/gif
+xdg-mime default org.kde.gwenview.desktop image/bmp
+xdg-mime default org.kde.gwenview.desktop image/tiff
+xdg-mime default org.kde.gwenview.desktop image/avif
 
 sed -i \
   -e 's|^TryExec=.*|TryExec=launch_nvim.sh|' \
@@ -33,5 +42,9 @@ EOF
 chmod +x ~/.local/bin/xdg-terminal-exec
 
 git update-index --skip-worktree .config/hypr/themes/colors.conf
+git lfs install
+git lfs pull
 
 sudo timedatectl set-local-rtc 1
+
+sed -i 's|\(^Exec=\)\(.*\)|\1env QT_QPA_PLATFORM=xcb \2|' /usr/share/applications/xppentablet.desktop
